1f05c49dafbafcd28a0fee666db6a2cd0cb7b9bc
wahyubrr <wahyu@synapsis.id>
2022-11-16 11:47:40 +0700

    revised algorithm, a more efficient one

--- testing/nmea-parser.c
+++ testing/nmea-parser.c
@@ -1,11 +1,10 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 
-char *parseNMEA(char *data, int index);
+char* parseNMEA(char* data, int index);
 
 int main() {
     // Write C code here
-    printf("Hello world\n");
     char data[6][100] = {
       "$GPGSV,8,1,25,21,44,141,47,15,14,049,44,6,31,255,46,3,25,280,44*75",
       "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A",
@@ -25,33 +24,58 @@ int main() {
     for (int i = 0; i < 6; i++) {
         printf("%s\n", data[i]);
     }
+    printf("\n");
     
-    char parsedData[21][20];
     //parsing
-    printf("\nParsed data: \n");
-    char *result = parseNMEA(data[0], 0);
-    printf("result: %s", result);
+    for (int j = 0; j < 6; j++) {
+        printf("%s\nParsed data: \n", data[j]);
+        char* res = parseNMEA(data[j], 0);
+        int i = 1;
+        while (res != NULL) {
+            printf("result: %s\n", res);
+            res = parseNMEA(data[j], i++);
+        }
+        printf("\n");
+    }
     
     return 0;
 }
 
-char *parseNMEA(char *data, int index) {
-    static char res[21][21];
-    int i = 0, row = 0, col = 0;
-    while(1) {
-        if (data[i] == '\0') {
-            break;   
-        } else if (data[i] == ',' || data[i] == '*') {
-            // printf("\n");
-            row++;
-            col = 0;
-        } else {
-            // printf("%c", data[i]);
-            res[row][col] = data[i];
-            col++;
+char* parseNMEA(char* data, int index) {
+    unsigned int n = strlen(data), lowerIndex, higherIndex, comma = 0;
+    int lowerFlag = 0, higherFlag = 0;
+    for (int i = 0; i < n; i++) {
+        if (comma == index && lowerFlag == 0) {
+            lowerIndex = i;
+            lowerFlag++;
         }
+        if (comma == (index + 1)) {
+            higherIndex = i - 1;
+            higherFlag++;
+            break;
+        }
+        if (data[i] == ',' || data[i] == '*') {
+            comma++;
+        }
+    }
+    // if at the end higher flag still zero, fill it with string length
+    if (higherFlag == 0) {
+        higherIndex = n;
+    }
+    // if the data is a parity data (last index), print the '*' tag too
+    if (lowerIndex != 0 && data[lowerIndex - 1] == '*') {
+        lowerIndex--;
+    }
+    // if at the end higher flag and lower flag not found, return NULL
+    // usually it is 
+    if (higherFlag == 0 && lowerFlag == 0) {
+        return NULL;
+    }
+    char* res = (char*)malloc((higherIndex-lowerIndex) * sizeof(char));
+    int i = 0;
+    for (int iData = lowerIndex; iData < higherIndex; iData++) {
+        res[i] = data[iData];
         i++;
     }
-    // printf("%s\n", res[0]);
-    return res[index];
+    return res;
 }
